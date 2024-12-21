37764203cac1b55366adac141f63c3e1af909367
anjali-dev22 <anjalirajput14316@gmail.com>
2024-12-21 21:46:56 +0530

    ok

--- js/script.js
+++ js/script.js
@@ -13,6 +13,7 @@ tailwind.config = {
       },
       fontSize: {
          '22': '22px',
+         '28': '28px',
         '34': '34px',
         '54': '54px'
       },
@@ -28,22 +29,40 @@ tailwind.config = {
 
 // Swiper Initialization
 const swiper = new Swiper(".productSwiper", {
-  slidesPerView: 4.5, // Number of slides visible
-  spaceBetween: 20, // Space between slides
+  slidesPerView: 4.5, // Default number of slides visible
+  spaceBetween: 20, // Default space between slides
   loop: true, // Enable looping
   grabCursor: true, // Enable grab cursor on hover
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    renderBullet: function (index, className) {
-      // Show only the first three dots
-      if (index < 3) {
-        return `<span class="${className}"></span>`;
-      }
-      return ""; // No dot for other slides
+  navigation: {
+    nextEl: ".swiper-button-next", // Target next arrow
+    prevEl: ".swiper-button-prev", // Target previous arrow
+  },
+  pagination: false,
+  // Responsive Breakpoints
+  breakpoints: {
+    // When window width is >= 320px
+    320: {
+      slidesPerView: 1.2,
+      spaceBetween: 10,
+    },
+    // When window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 15,
+    },
+    // When window width is >= 768px
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    // When window width is >= 1024px
+    1024: {
+      slidesPerView: 4.5,
+      spaceBetween: 20,
     },
   },
 });
+
 
 
       // Initialize Swiper for Thumbnails
@@ -63,5 +82,67 @@ const swiperMain = new Swiper(".mySwiper2", {
   thumbs: {
     swiper: swiperThumbs, // Connect to thumbnail Swiper
   },
+});
+
+
+// Swiper Initialization
+const brandsSwiper = new Swiper(".brands-slider", {
+  slidesPerView: 6, // Default number of slides visible
+  spaceBetween: 10, // Default space between slides
+  loop: true, // Enable looping
+  grabCursor: true, // Enable grab cursor on hover
+  navigation: {
+    nextEl: ".swiper-button-next", // Target next arrow
+    prevEl: ".swiper-button-prev", // Target previous arrow
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+   // Responsive Breakpoints
+   breakpoints: {
+    // When window width is >= 320px
+    320: {
+      slidesPerView: 2.1,
+      spaceBetween: 10,
+    },
+    // When window width is >= 480px
+    480: {
+      slidesPerView: 2.7,
+      spaceBetween: 15,
+    },
+    // When window width is >= 768px
+    768: {
+      slidesPerView: 3.5,
+      spaceBetween: 20,
+    },
+    // When window width is >= 1024px
+    1024: {
+      slidesPerView: 4.5,
+      spaceBetween: 20,
+    },
+     // When window width is >= 1180
+     1180: {
+      slidesPerView: 6,
+      spaceBetween: 20,
+    },
+  },
+});
+
+const menu = document.getElementById("menu-div");
+const toggleDiv = document.getElementById("toggleDiv");
+
+menu.addEventListener("click", () => {
+  // Toggle the 'hidden' class on click
+  toggleDiv.classList.toggle("visible");
+  menu.classList.toggle('active');
+});
+
+const cart = document.getElementById("cart-button");
+const sidebar = document.getElementById("cartSlider");
+
+cart.addEventListener("click", () => {
+  // Toggle the 'hidden' class on click
+  sidebar.classList.toggle("hidden");
 });
 
